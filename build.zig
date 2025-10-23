f33995dc2c2ccedfc95ab9152716fa04f0b3d8e8
Lorenzo Polverelli <lorenzo.polverelli13@gmail.com>
2025-10-23 20:55:15 +0200

    remove not working flags for zig cc

--- build.zig
+++ build.zig
@@ -4,29 +4,22 @@ pub fn build(b: *std.Build) void {
     const mod = b.createModule(.{
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
-		.link_libc = true,
+        .link_libc = true,
     });
 
     mod.addCSourceFiles(.{
         .root = b.path("src"),
         .files = &.{"main.c"},
         .language = .c,
-        .flags = &.{
-			"-std=c23",
-			"-Wall",
-			"-Wextra",
-			"-Wno-format",
-			"-Wno-sign-compare",
-			"-Wconversion",
-		},
+        .flags = &.{"-std=c23"},
     });
 
     mod.linkSystemLibrary("opengl32", .{});
     mod.linkSystemLibrary("gdi32", .{});
     mod.linkSystemLibrary("winmm", .{});
 
-	mod.addIncludePath(b.path("raylib/include"));
-	mod.addObjectFile(b.path("raylib/lib/libraylib.a"));
+    mod.addIncludePath(b.path("raylib/include"));
+    mod.addObjectFile(b.path("raylib/lib/libraylib.a"));
 
     const exe = b.addExecutable(.{
         .name = "snake",
